4f04372075322125aa6904eba77e8474c68503f5
chefinDan <greendan@oregonstate.edu>
2020-04-15 19:35:14 -0700

    fix repo issues

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+const VARIANT_URL = new URL('https://cfw-takehome.developers.workers.dev/api/variants');
+var urls = [];
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(chooseRandomUrl(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+
+async function chooseRandomUrl(request) {
+  const rand = Math.round(Math.random())
+  try{
+    if(urls.length === 0)
+      await fetchVariantUrls()
+    return await fetch(urls[rand]);
+
+  } catch(err){
+    return new Response("Error fetching resource at: " + VARIANT_URL)
+  }
+}
+
+async function fetchVariantUrls(){
+  console.log('Fetching Variant URLS...')
+  const fetch_response = await fetch(VARIANT_URL);
+  if(fetch_response.status !== 200)
+    throw new Error(fetch_response.status);
+  const body = await fetch_response.json();
+  urls.push(...body.variants);
 }
